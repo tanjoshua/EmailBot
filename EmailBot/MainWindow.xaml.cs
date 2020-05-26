77ba3392808f6025d3f3bd190c7cb12444dfd847
Joshua Tan <jtanjoshua@gmail.com>
2020-05-26 10:09:08 +0800

    send emails

--- EmailBot/MainWindow.xaml.cs
+++ EmailBot/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Net.Mail;
 
 namespace EmailBot
 {
@@ -41,7 +42,22 @@ namespace EmailBot
                 emailAddress = "joshua.tan@themeinternationaltrading.com";
             }
 
-            MessageBox.Show($"Email {content} sent to {emailAddress}");
+            MailMessage mail = new MailMessage();
+            mail.From = new MailAddress("joshua.tan@themeinternationaltrading.com");
+            mail.To.Add(emailAddress);
+            mail.Subject = "Email bot";
+            mail.Body = content + "\n\nThis is an email sent from a bot";
+
+            SmtpClient SmtpServer = new SmtpClient("smtp.gmail.com");
+            SmtpServer.UseDefaultCredentials = false;
+            SmtpServer.Port = 25;
+            SmtpServer.Credentials = new System.Net.NetworkCredential("joshua.tan@themeinternationaltrading.com",
+                "<REDACTED>");
+            SmtpServer.EnableSsl = true;
+
+            SmtpServer.Send(mail);
+
+            MessageBox.Show($"Email sent to {emailAddress}");
         }
 
         private void CancelButton_Click(object sender, RoutedEventArgs e)
